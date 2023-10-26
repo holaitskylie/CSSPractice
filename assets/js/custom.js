21b436bcd63134194d31359fb0d3e2de77ac4e66
holaitskylie <june2840@naver.com>
2023-10-26 14:48:28 +0900

    Test

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,7 +27,7 @@
       hour = minute * 60,
       day = hour * 24;
 
-	let countDown = new Date('Mar 31, 2022 09:30:00').getTime(),
+	let countDown = new Date('Dec 31, 2023 09:30:00').getTime(),
     x = setInterval(function() {    
 
       let now = new Date().getTime(),
